aeeef4466354d1c3cb9826bc4b31da48f3f5573c
sparktseung <sparkingca@gmail.com>
2020-08-01 13:12:00 -0400

    CRAN check

--- tests/testthat/test-em-lognormal.R
+++ tests/testthat/test-em-lognormal.R
@@ -57,50 +57,50 @@ test_that("EMMLognormal", {
 # sdlog = 1
 #
 # censor.idx = (tempY[,2]!=tempY[,3])
-
-data("LRMoEDemoData")
-head(X)
-
-alpha = matrix(c(0.5, 0.25, -0.05, 0.3, -0.2,
-                 0, 0, 0, 0, 0),
-               nrow = 2, byrow = T)
-
-head(exp(GateLogit(X, alpha)))
-hist(exp(GateLogit(X, alpha))[,1])
-
-comp.dist = matrix(c("lnorm", "ZI-lnorm"),
-                   nrow = 1, byrow = T)
-
-zero.prob = matrix(c(0, 0.10),
-                   nrow = 1, byrow = T)
-
-params.list = list(list(c(3, 0.25), c(1, 0.5)))
-
-simy = SimYSet(X, alpha, comp.dist, zero.prob, params.list)
-
-hist(simy, breaks = 200, xlim = c(0, 50))
-
-YY = cbind(rep(0, nrow(X)), simy, simy, rep(Inf, nrow(X)))
-
-YY[c(251:500),2] = simy[c(250:500)] * 0.75
-YY[c(501:750),3] = simy[c(250:500)] * 1.50
-
-YY[c(751:1000),1] = simy[c(751:1000)] * 0.25
-
-YY[c(1001:1250),4] = simy[c(1001:1250)] * 2
-
-YY[c(1251:1500),1] = simy[c(1251:1500)] * 0.25
-YY[c(1251:1500),2] = simy[c(1251:1500)] * 0.75
-YY[c(1251:1500),3] = simy[c(1251:1500)] * 1.25
-YY[c(1251:1500),4] = simy[c(1251:1500)] * 2
-
-
-alpha.init = alpha = matrix(c(0, 0, 0, 0, 0,
-                              0, 0, 0, 0, 0),
-                            nrow = 2, byrow = T)
-comp.dist = comp.dist
-zero.init = matrix(c(0, 0.50),
-                   nrow = 1, byrow = T)
-params.init = list(list(c(2.5, 0.1), c(2, 0.8)))
-
-modelfit = LRMoEFit(YY, X, 2, comp.dist, alpha.init, zero.init, params.init)
+#
+# data("LRMoEDemoData")
+# head(X)
+#
+# alpha = matrix(c(0.5, 0.25, -0.05, 0.3, -0.2,
+#                  0, 0, 0, 0, 0),
+#                nrow = 2, byrow = T)
+#
+# head(exp(GateLogit(X, alpha)))
+# hist(exp(GateLogit(X, alpha))[,1])
+#
+# comp.dist = matrix(c("lnorm", "ZI-lnorm"),
+#                    nrow = 1, byrow = T)
+#
+# zero.prob = matrix(c(0, 0.10),
+#                    nrow = 1, byrow = T)
+#
+# params.list = list(list(c(3, 0.25), c(1, 0.5)))
+#
+# simy = SimYSet(X, alpha, comp.dist, zero.prob, params.list)
+#
+# hist(simy, breaks = 200, xlim = c(0, 50))
+#
+# YY = cbind(rep(0, nrow(X)), simy, simy, rep(Inf, nrow(X)))
+#
+# YY[c(251:500),2] = simy[c(250:500)] * 0.75
+# YY[c(501:750),3] = simy[c(250:500)] * 1.50
+#
+# YY[c(751:1000),1] = simy[c(751:1000)] * 0.25
+#
+# YY[c(1001:1250),4] = simy[c(1001:1250)] * 2
+#
+# YY[c(1251:1500),1] = simy[c(1251:1500)] * 0.25
+# YY[c(1251:1500),2] = simy[c(1251:1500)] * 0.75
+# YY[c(1251:1500),3] = simy[c(1251:1500)] * 1.25
+# YY[c(1251:1500),4] = simy[c(1251:1500)] * 2
+#
+#
+# alpha.init = alpha = matrix(c(0, 0, 0, 0, 0,
+#                               0, 0, 0, 0, 0),
+#                             nrow = 2, byrow = T)
+# comp.dist = comp.dist
+# zero.init = matrix(c(0, 0.50),
+#                    nrow = 1, byrow = T)
+# params.init = list(list(c(2.5, 0.1), c(2, 0.8)))
+#
+# modelfit = LRMoEFit(YY, X, 2, comp.dist, alpha.init, zero.init, params.init)
