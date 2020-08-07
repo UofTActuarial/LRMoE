1d75170659548fc2292b954852fedde4032f28c9
sparktseung <sparkingca@gmail.com>
2020-08-07 04:57:57 -0400

    Added PredictVariance.R

--- R/PredictVariance.R
+++ R/PredictVariance.R
@@ -1,15 +1,16 @@
-#' A function to calculate the mean of a matrix of given distributions (positive part only), by dimension and by component.
+#' A switch to calculate the variance of a matrix of given distributions (positive part only), by dimension and by component.
 #'
 #' @param comp.dist A d*g matrix of strings, describing component distributions by dimension and by component.
 #' @param paramas.list A list of length d, where each element is a sublist of length g.
 #'                    Each sublist contains one numeric vector, which is the parameter value for the corresponding \code{comp.dist}.
 #'
-#' @return A matrix of mean values by dimension and by component.
+#'
+#' @return A matrix of variance by dimension and by component.
 #'
 #' @keywords internal
 #'
-# #' @export DimCompPosYMean
-DimCompPosYMean = function(comp.dist, params.list)
+# #' @export DimCompPosYVar
+DimCompPosYVar = function(comp.dist, params.list)
 {
   n.comp = ncol(comp.dist)
   dim.m = nrow(comp.dist)
@@ -20,7 +21,7 @@ DimCompPosYMean = function(comp.dist, params.list)
   {
     for(j in 1:n.comp)
     {
-      result[k, j] = PosMean(comp.dist[k,j], params.list[[k]][[j]])
+      result[k, j] = PosVariance(comp.dist[k,j], params.list[[k]][[j]])
     }
   }
 
@@ -28,7 +29,7 @@ DimCompPosYMean = function(comp.dist, params.list)
 }
 
 
-#' A function to calculate the mean of a matrix of given distributions (with zero inflation), by dimension and by component.
+#' A function to calculate the variance of a matrix of given distributions (with zero inflation), by dimension and by component.
 #'
 #' @param comp.dist A d*g matrix of strings, describing component distributions by dimension and by component.
 #' @param zero.prob A d*g matrix of numbers between 0 and 1, describing zero probability masses by dimension and by component.
@@ -36,21 +37,23 @@ DimCompPosYMean = function(comp.dist, params.list)
 #'                    Each sublist contains one numeric vector, which is the parameter value for the corresponding \code{comp.dist}.
 #'
 #'
-#' @return A matrix of mean values by dimension and by component.
+#' @return A matrix of variance by dimension and by component.
 #'
 #' @keywords internal
 #'
-#' @export DimCompYMean
-DimCompYMean = function(comp.dist, zero.prob, params.list)
+# #' @export DimCompYVar
+DimCompYVar = function(comp.dist, zero.prob, params.list)
 {
-  temp = DimCompPosYMean(comp.dist, params.list)
-  result = (1-zero.prob)*temp
+  cond.mean = DimCompPosYMean(comp.dist, params.list)
+  cond.var = DimCompPosYVar(comp.dist, params.list)
+
+  result = (1-zero.prob)*cond.var + zero.prob*(1-zero.prob)*cond.mean^2
 
   return(result)
 }
 
 
-# #' Predict the mean of y, given a fixed covariate vector X and a model.
+# #' Predict the variance of y, given a fixed covariate vector X and a model.
 # #'
 # #' @param X A matrix of covariates.
 # #' @param alpha A matrix of logit regression coefficients.
@@ -60,26 +63,35 @@ DimCompYMean = function(comp.dist, zero.prob, params.list)
 # #' @param params.list A list of length d, where each element is a sublist of length g.
 # #'                    Each sublist contains one numeric vector, which is the parameter value for the corresponding \code{comp.dist}.
 # #'
+# #' @seealso \code{\link{LRMoE.fit}}.
 # #'
-# #' @return A vector of mean values by dimension.
+# #' @return A vector of variance by dimension.
 # #'
 # #' @keywords internal
 # #'
-# #' @export ind.predict.mean
-# ind.predict.mean = function(X, alpha, comp.dist, zero.prob, params.list)
+# #' @export ind.predict.var
+# ind.predict.var = function(X, alpha, comp.dist, zero.prob, params.list)
 # {
 #   n.comp = nrow(alpha)
 #   dim.m = nrow(comp.dist)
 #
 #   weighting = exp(gate.logit(X, alpha))
-#   temp = dim.comp.mean.y(comp.dist, zero.prob, params.list)
-#   result = temp%*%t(weighting)
 #
-#   return(t(result))
+#   # Var of conditional expectation
+#   cond.mean = dim.comp.mean.y(comp.dist, zero.prob, params.list)
+#   cond.mean.squared = cond.mean^2
+#   grand.mean = cond.mean%*%t(weighting)
+#   var.cond.mean = cond.mean.squared%*%t(weighting) - (grand.mean)^2
+#
+#   # Mean of conditional variance
+#   cond.var = dim.comp.var.y(comp.dist, zero.prob, params.list)
+#   ave.cond.var = cond.var%*%t(weighting)
+#
+#   return(t(var.cond.mean + ave.cond.var))
 # }
 
 
-#' Predict the mean of y, given a fixed covariate matrix X and a model.
+#' Predict the variance of y, given a fixed covariate matrix X and a model.
 #'
 #' @param X A matrix of covariates.
 #' @param alpha A matrix of logit regression coefficients.
@@ -90,30 +102,37 @@ DimCompYMean = function(comp.dist, zero.prob, params.list)
 #'                    Each sublist contains one numeric vector, which is the parameter value for the corresponding \code{comp.dist}.
 #'
 #'
-#' @return A matrix of mean values by observation and by dimension.
+#' @return A matrix of variance by observation and by dimension.
 #'
-#' @export PredictMeanPrior
-PredictMeanPrior = function(X, alpha, comp.dist, zero.prob, params.list)
+#'
+#' @export PredictVariancePrior
+PredictVariancePrior = function(X, alpha, comp.dist, zero.prob, params.list)
 {
   X.size = nrow(X)
   dim.m = nrow(comp.dist)
+
   result = array(0, dim = c(X.size, dim.m))
 
   meantmp = DimCompYMean(comp.dist, zero.prob, params.list)
+  vartmp = DimCompYVar(comp.dist, zero.prob, params.list)
   weighting = PredictClassPrior(X, alpha, TRUE)
 
   for(d in 1:dim.m){
-    result[,d] = tcrossprod(weighting, t(meantmp[d,]))
+    if(sum(meantmp[d,])==Inf){ # Some component has infinite mean
+      result[,d] = Inf
+    }else{
+      ee = tcrossprod(weighting, t(meantmp[d,]))
+      result[,d] = tcrossprod(weighting, (t(meantmp[d,]))^2 + t(vartmp[d,])) - (ee)^2
+    }
   }
 
   return(result)
 
 }
 
-
-#' Predict the posterior mean of y, given a matrix of observations Y, a fixed covariate matrix X and a model.
+#' Predict the posterior variance of y, given a matrix of observations Y, a fixed covariate matrix X and a model.
 #'
-#' @param Y A matrix of observed responses for \code{X}.
+#'@param Y A matrix of observed responses for \code{X}.
 #' @param X A matrix of covariates.
 #' @param alpha A matrix of logit regression coefficients.
 #' @param comp.dist A d*g matrix of strings, which specify the component distributions to fit.
@@ -123,20 +142,28 @@ PredictMeanPrior = function(X, alpha, comp.dist, zero.prob, params.list)
 #'                    Each sublist contains one numeric vector, which is the parameter value for the corresponding \code{comp.dist}.
 #'
 #'
-#' @return A matrix of mean values by observation and by dimension.
+#' @return A matrix of variance by observation and by dimension.
 #'
-#' @export PredictMeanPosterior
-PredictMeanPosterior = function(Y, X, alpha, comp.dist, zero.prob, params.list)
+#'
+#' @export PredictVariancePosterior
+PredictVariancePosterior = function(Y, X, alpha, comp.dist, zero.prob, params.list)
 {
   X.size = nrow(X)
   dim.m = nrow(comp.dist)
+
   result = array(0, dim = c(X.size, dim.m))
 
   meantmp = DimCompYMean(comp.dist, zero.prob, params.list)
+  vartmp = DimCompYVar(comp.dist, zero.prob, params.list)
   weighting = PredictClassPosterior(Y, X, alpha, comp.dist, zero.prob, params.list, TRUE)
 
   for(d in 1:dim.m){
-    result[,d] = tcrossprod(weighting, t(meantmp[d,]))
+    if(sum(meantmp[d,])==Inf){ # Some component has infinite mean
+      result[,d] = Inf
+    }else{
+      ee = tcrossprod(weighting, t(meantmp[d,]))
+      result[,d] = tcrossprod(weighting, (t(meantmp[d,]))^2 + t(vartmp[d,])) - (ee)^2
+    }
   }
 
   return(result)
--- tests/test-loglik.R
+++ tests/test-loglik.R
@@ -1,23 +1,23 @@
-data(LRMoEDemoData)
-
-head(X)
-head(Y)
-
-alpha.init = matrix(runif(15, -1, 1), nrow = 3, byrow = T)
-comp.dist = matrix(c("poisson", "nbinom", "ZI-gammacount",
-                     "lnorm", "ZI-burr", "invgauss"),
-                   nrow = 2, byrow = T)
-zero.init = matrix(c(0, 0, 0.5,
-                     0, 0.5, 0),
-                   nrow = 2, byrow = T)
-params.init = list(
-  list(c(10), c(10, 0.5), c(5, 5)),
-  list(c(2, 1), c(1, 2, 10), c(10, 10))
-)
-params.pen = list(
-  list(c(2, 1), c(2, 1), c(2, 1, 2, 1)),
-  list(c(Inf, 1, Inf), c(2, 1, 2, 1, 2, 1), c(1, Inf, 1, Inf))
-)
+# data(LRMoEDemoData)
+#
+# head(X)
+# head(Y)
+#
+# alpha.init = matrix(runif(15, -1, 1), nrow = 3, byrow = T)
+# comp.dist = matrix(c("poisson", "nbinom", "ZI-gammacount",
+#                      "lnorm", "ZI-burr", "invgauss"),
+#                    nrow = 2, byrow = T)
+# zero.init = matrix(c(0, 0, 0.5,
+#                      0, 0.5, 0),
+#                    nrow = 2, byrow = T)
+# params.init = list(
+#   list(c(10), c(10, 0.5), c(5, 5)),
+#   list(c(2, 1), c(1, 2, 10), c(10, 10))
+# )
+# params.pen = list(
+#   list(c(2, 1), c(2, 1), c(2, 1, 2, 1)),
+#   list(c(Inf, 1, Inf), c(2, 1, 2, 1, 2, 1), c(1, Inf, 1, Inf))
+# )
 
 # gate.ll = GateLogit(X.obs, alpha.init)
 # expert.list = DimCompExpertLL(Y.obs, comp.dist, zero.init, params.init)
@@ -30,8 +30,11 @@ params.pen = list(
 # temp3 = PredictClassPosterior(Y, X, alpha.init, comp.dist, zero.init, params.init, T)
 # temp4 = PredictClassPosterior(Y, X, alpha.init, comp.dist, zero.init, params.init, F)
 
-temp1 = PredictMeanPrior(X, alpha.init, comp.dist, zero.init, params.init)
-temp2 = PredictMeanPosterior(Y, X, alpha.init, comp.dist, zero.init, params.init)
+# temp1 = PredictMeanPrior(X, alpha.init, comp.dist, zero.init, params.init)
+# temp2 = PredictMeanPosterior(Y, X, alpha.init, comp.dist, zero.init, params.init)
+
+# temp1 = PredictVariancePrior(X, alpha.init, comp.dist, zero.init, params.init)
+# temp2 = PredictVariancePosterior(Y, X, alpha.init, comp.dist, zero.init, params.init)
 
 # Returns numbers!
 
