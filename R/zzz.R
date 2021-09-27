606e36d1aa59b44204e09d4a9804a1a6be160c7c
sparktseung <hi@sparktseung.com>
2021-09-26 21:02:23 -0400

    Document demo data

--- R/zzz.R
+++ R/zzz.R
@@ -39,3 +39,28 @@
   .expertlib$register(expert_name = "ziinversegaussian", continuous = TRUE)
   .expertlib$register(expert_name = "ziweibull", continuous = TRUE)
 }
+
+
+#' Demo data for \code{LRMoE}.
+#'
+#' \itemize{
+#'   \item \code{X}: A matrix of covariates required by the LRMoE fitting function (complete).
+#'   \item \code{Y}: A matrix of response required by the LRMoE fitting function (complete).
+#'   \item \code{X.obs}: A matrix of covariates required by the LRMoE fitting function (after truncation and censoring).
+#'   \item \code{Y.obs}: A matrix of response required by the LRMoE fitting function (after truncation and censoring).
+#' }
+#'
+#' @docType data
+#' @keywords data
+#' @name LRMoEDemoData
+#'
+'X'
+
+#' @rdname LRMoEDemoData
+'Y'
+
+#' @rdname LRMoEDemoData
+'X.obs'
+
+#' @rdname LRMoEDemoData
+'Y.obs'
